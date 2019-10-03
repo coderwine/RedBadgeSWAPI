006eff43d1cc4ac1b786fbd9d78c0cab368203b6
Sam DeKlyen <samdeklyen@gmail.com>
2019-10-03 11:44:58 -0400

    f

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SwapiService} from '../swapi.service';
+import {People} from '../people'
 
 @Component({
   selector: 'app-table',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  characters=People;
 
-  constructor() { }
+  constructor(private SwapiService: SwapiService) { }
 
   ngOnInit() {
+    this.SwapiService.getPeople()
+    .subscribe((data) => {
+      this.characters = data['characters'];
+      console.log(data)
+    })
   }
 
 }
