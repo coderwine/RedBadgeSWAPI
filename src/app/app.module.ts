95b8acdf2337f81015b35858c522e86be20ab0a3
Eric <ejwinebrenner@gmail.com>
2019-10-03 10:02:43 -0400

    pull from master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app.material.module';
 import { DropdownComponent } from './dropdown/dropdown.component';
 
-
-import { DrawerComponent } from './drawer/drawer.component';
-
-
 import { HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './search/search.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -30,21 +26,12 @@ import { MatInputModule } from '@angular/material/input';
   imports: [
     BrowserModule,
     BrowserAnimationsModule, 
-    AppMaterialModule
-
+    AppMaterialModule,
+    HttpClientModule,
+    MatInputModule,
     CharactersComponent,
     SearchComponent,
     MessagesComponent
-  ],
-  imports: [
-    BrowserModule,
-
-    BrowserAnimationsModule
-
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatInputModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
